8fd7e7ab2e610bb7291d5d6eb07a95844db6c731
Neil Matatall <neilm@twitter.com>
2013-09-04 18:26:39 -0700

    Some cleanup

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,6 @@ Bundler.require(:default, Rails.env)
 
 module ScriptHash
   class Application < Rails::Application
-    # config.middleware.use SecureHeaders::ScriptHash
+    config.action_dispatch.default_headers.clear
   end
 end
